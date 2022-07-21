565d2bc22feaa086a39ee7377042cdad1899ee8f
anassvoronn <UQzbwQCt7R3uMfz>
2022-07-21 18:17:37 +0300

    Fixed homework

--- src/main/java/org/dz/solution/Subsequence.java
+++ src/main/java/org/dz/solution/Subsequence.java
@@ -8,17 +8,22 @@ public class Subsequence {
         if (number == 1) {
             return 0;
         }
+        if (number == 2) {
+            return 1;
+        }
+        if (number == 3) {
+            return 2;
+        }
         int elem1 = 0;
         int elem2 = 1;
         int elem3 = 2;
         int elem4 = elem3 * 2 + elem1;
 
-        for (int i = number - 2; i >= 0; i--) {
+        for (int i = number - 4; i > 0; i--) {
             elem1 = elem2;
             elem2 = elem3;
             elem3 = elem4;
             elem4 = elem3 * 2 + elem1;
-            if (i == 0) break;
         }
         return elem4;
     }
--- src/test/java/org/dz/solution/SubsequenceTest.java
+++ src/test/java/org/dz/solution/SubsequenceTest.java
@@ -3,7 +3,6 @@ package org.dz.solution;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class SubsequenceTest {
 
@@ -24,14 +23,35 @@ public class SubsequenceTest {
     public void getNumberByFormulaOf19() {
         Subsequence calculator = new Subsequence();
         int result = calculator.getNumberByFormula(19);
-        Assert.assertEquals(6254320, result);
+        Assert.assertEquals(582932, result);
     }
 
     @Test
     public void getNumberByFormulaOf10() {
         Subsequence calculator = new Subsequence();
         int result = calculator.getNumberByFormula(10);
-        Assert.assertEquals(5064, result);
+        Assert.assertEquals(472, result);
+    }
+
+    @Test
+    public void getNumberByFormulaOf2() {
+        Subsequence calculator = new Subsequence();
+        int result = calculator.getNumberByFormula(2);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void getNumberByFormulaOf3() {
+        Subsequence calculator = new Subsequence();
+        int result = calculator.getNumberByFormula(3);
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void getNumberByFormulaOf4() {
+        Subsequence calculator = new Subsequence();
+        int result = calculator.getNumberByFormula(4);
+        Assert.assertEquals(4, result);
     }
 
 }
